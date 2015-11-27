139bd947ec227c0d5657d641ab76aeebe0c38986
Santiago Palladino <spalladino@gmail.com>
2015-11-27 12:52:34 -0300

    Fixed issue when comparing guessed labels to test labels (training labels were being used). Added logging to stdout. Limit number of cases loaded.

--- examples/mnist/mnist.cr
+++ examples/mnist/mnist.cr
@@ -10,12 +10,18 @@ TRAIN_LABELS_PATH = "data/train-labels"
 TEST_IMAGES_PATH  = "data/test-images"
 TEST_LABELS_PATH  = "data/test-labels"
 
-def read_labels(path)
+TRAINING_SIZE = 6000 # up to 60000
+TESTING_SIZE  = 1000 # up to 10000
+
+
+def read_labels(path, count)
   File.open(path, "rb") do |file|
     magic = file.read_bytes(Int32, IO::ByteFormat::BigEndian)
     raise "Invalid magic value: read #{magic}, expected 2049" unless magic == 2049
 
     nitems = file.read_bytes(Int32, IO::ByteFormat::BigEndian)
+    nitems = [nitems, count].min
+
     labels = Array(UInt8).new(nitems)
     nitems.times do
       labels << file.read_byte.not_nil!
@@ -25,7 +31,7 @@ def read_labels(path)
   end
 end
 
-def read_images(path)
+def read_images(path, count)
   File.open(path, "rb") do |file|
     magic = file.read_bytes(Int32, IO::ByteFormat::BigEndian)
     raise "Invalid magic value: read #{magic}, expected 2051" unless magic == 2051
@@ -33,6 +39,8 @@ def read_images(path)
     nimgs = file.read_bytes(Int32, IO::ByteFormat::BigEndian)
     nrows = file.read_bytes(Int32, IO::ByteFormat::BigEndian)
     ncols = file.read_bytes(Int32, IO::ByteFormat::BigEndian)
+
+    nimgs = [nimgs, count].min
 
     imgs = Array(Array(UInt8)).new(nimgs)
     nimgs.times do
@@ -46,9 +54,9 @@ def read_images(path)
   end
 end
 
-def load(img_path, label_path)
-  imgs, nrows, ncols = read_images(img_path)
-  labels = read_labels(label_path)
+def load(img_path, label_path, count)
+  imgs, nrows, ncols = read_images(img_path, count)
+  labels = read_labels(label_path, count)
   {Matrix.rows(imgs), labels}
 end
 
@@ -62,26 +70,33 @@ def distance(array_1, array_2)
 end
 
 # Load train data
-images, labels = load(TRAIN_IMAGES_PATH, TRAIN_LABELS_PATH)
+images, labels = load(TRAIN_IMAGES_PATH, TRAIN_LABELS_PATH, TRAINING_SIZE)
+puts "Loaded #{labels.size} images for training"
 
 # Normalise by number of images
 div = Math.sqrt(images.number_of_rows - 1)
 images.each_by_col do |value, i, j|
   images[i, j] = value / div
 end
+puts "Normalised training set"
 
 # Run PCA on the training set
 pca = images.pca_fit(PCA_N_COMPONENTS)
+puts "PCA fit with #{PCA_N_COMPONENTS} on the training set"
 reduced_train_images = pca.transform
+puts "Transformed the training set using the PCA fit"
 
 # Load test data and apply same normalisation as training set
-test_images, test_labels = load(TEST_IMAGES_PATH, TEST_LABELS_PATH)
+test_images, test_labels = load(TEST_IMAGES_PATH, TEST_LABELS_PATH, TESTING_SIZE)
+puts "\nLoaded #{labels.size} images for testing"
 test_images.each_by_col do |value, i, j|
   test_images[i, j] = value / div
 end
+puts "Normalised testing set"
 
 # Apply PCA trained transformation on test data
 reduced_test_images = pca.transform(test_images)
+puts "Transformed the testing set using the PCA fit"
 
 # Guess each label based on the closest point from the training set
 guessed_labels = Array(UInt8).new(test_labels.size)
@@ -99,5 +114,5 @@ reduced_test_images.each_row do |test_image, i_test|
 end
 
 # Compute percentage of correct guesses
-n_correct = labels.zip(guessed_labels).count { |x| x[0] == x[1] }
-puts "Correct answers: #{n_correct} / #{labels.size} (#{n_correct / labels.size})"
+n_correct = test_labels.zip(guessed_labels).count { |x| x[0] == x[1] }
+puts "\nCorrect answers: #{n_correct} / #{test_labels.size} (#{n_correct.to_f / test_labels.size.to_f})"
