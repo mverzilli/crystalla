fca1991333d1d784d47b974496d33589fa5202b2
Santiago Palladino <spalladino@gmail.com>
2015-11-29 16:49:24 -0300

    Display LAPACK version when running specs

--- src/crystalla.cr
+++ src/crystalla.cr
@@ -1,5 +1,15 @@
 require "./crystalla/*"
 
 module Crystalla
-  # TODO Put your code here
+  def self.lapack_version
+    major :: Int32
+    minor :: Int32
+    patch :: Int32
+    LibLapack.ilaver(pointerof(major), pointerof(minor), pointerof(patch))
+    {major, minor, patch}
+  end
+
+  def self.display_info!
+    puts "Using LAPACK version #{Crystalla.lapack_version.to_a.join('.')}"
+  end
 end
--- src/crystalla/lib_lapack.cr
+++ src/crystalla/lib_lapack.cr
@@ -1,6 +1,7 @@
 module Crystalla
   @[Link(framework: "Accelerate")]
   lib LibLapack
+    fun ilaver(major : Int32*, minor : Int32*, patch : Int32*)
     fun lu = dgetrf_(m : Int32*, n : Int32*, a : Float64*, lda : Int32*, ipiv : Int32*, info : Int32*)
     fun dgetri_(n : Int32*, a : Float64*, lda : Int32*, ipiv : Int32*, work : Float64*, lwork : Int32*, info : Int32*)
     fun dgesv(n : Int32*, nhrs : Int32*, a : Float64*, lda : Int32*, ipiv : Int32*, b : Float64*, ldb : Int32*, info : Int32*)
